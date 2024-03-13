3e432925699d815cef62e725e111bf63329d1ac9
Matvey <resstas76@yandex.ru>
2024-03-13 20:01:51 +0300

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const content = document.querySelector('.content');
+const cardPlaces = content.querySelector('.places__list');
 
-// @todo: DOM узлы
+initialCards.forEach(function (addCard) {
+  
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  cardElement.querySelector('.card__image').src = addCard.link;
+  cardElement.querySelector('.card__title').textContent = addCard.name;
 
-// @todo: Функция создания карточки
+  const deleteIcon = cardElement.querySelector('.card__delete-button');
+  deleteIcon.addEventListener('click', deleteCard);
 
-// @todo: Функция удаления карточки
+  cardPlaces.append(cardElement);
+})
 
-// @todo: Вывести карточки на страницу
+function deleteCard(evt) {
+  const deleteBtn = evt.target.closest('.card');
+  deleteBtn.remove();
+}
+
+addCard();
